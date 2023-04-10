87faf631e392ee4976c95568ccb1d788aaf43b7e
Alex <gundorovsasha@gmail.com>
2023-04-10 15:06:39 +0300

    Добавьте файлы проекта.

---
+++ ConcreteObservable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeObserver
+{
+    internal class ConcreteObservable: IObservable
+    {
+        // список наблюдателей
+        private List<IObserver> observers;
+        public ConcreteObservable() {
+            observers = new List<IObserver> ();
+        }
+
+        // добавление объекта, который наблюдает
+        public void AddObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+        // удаление объекта, который наблюдает
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+        // оповещение объекта, который наблюдает 
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+                observer.Update();
+        }
+    }
+}
---
+++ IObservable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeObserver
+{
+    internal interface IObservable
+    {
+        // добавление наблюдателя
+        void AddObserver(IObserver o);
+        // удаление наблюдателя
+        void RemoveObserver(IObserver o);
+        // оповещение наблюдателя
+        void NotifyObservers();
+    }
+}
